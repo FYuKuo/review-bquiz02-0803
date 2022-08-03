8a03b2056a1b135eb353e2f27a454437270d7c51
FYKuo <teresa99339@gmail.com>
2022-08-03 14:13:46 +0800

    新增base檔案 建立物件導向sql語法

--- index.php
+++ index.php
@@ -41,9 +41,9 @@
                     <span style="width:18%; display:inline-block;">
                         <a href="?do=login">會員登入</a>
                     </span>
-                    <div class="">
-                    </div>
                 </div>
+				<div class="">
+				</div>
             </div>
         </div>
         <div id="bottom">
